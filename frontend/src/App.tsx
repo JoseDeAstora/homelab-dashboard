f9aef402306d486ea86f2eed22651a5e1ad55f65
valletjo <jose.vallet.comes@uni-a.de>
2026-05-22 11:31:28 +0200

    testing cloudflare

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,11 +18,11 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const resMetrics = await fetch('http://localhost:3000/api/metrics');
+        const resMetrics = await fetch('https://gentle-publicity-saint-feof.trycloudflare.com/api/metrics');
         const dataMetrics = await resMetrics.json();
         setMetrics(dataMetrics);
 
-        const resHistory = await fetch('http://localhost:3000/api/history');
+        const resHistory = await fetch('https://gentle-publicity-saint-feof.trycloudflare.com/api/history');
         const dataHistory = await resHistory.json();
         
         const formattedHistory = dataHistory.reverse().map((item: any) => ({
